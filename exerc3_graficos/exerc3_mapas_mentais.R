111f8166fe1f1a7ed483a88208ce3f565755804d
Rafael <rafaelmoreiracvo@yahoo.com.br>
2019-12-16 07:40:40 -0300

    trablhos finais

---
+++ exerc3_graficos/exerc3_mapas_mentais.R
@@ -0,0 +1,17 @@
+# 1. Crie um mapa mental para cada um dos textos: Creating Visual Representations e Perception. 
+#    Os diagramas deverão ser entregues em um arquivo no formato pdf.
+
+
+
+
+  
+
+
+
+
+
+  # 2. [Desafio] Usando a linguagem R ou a biblioteca D3 (https://d3js.org/) 
+#    crie as técnicas de visualização de dados abaixo:
+# A. Mapa: dê preferência para dados do Brasil;
+# B. Visualização de Rede: utilize dados multivariados, ou seja, mapeie colunas do 
+#    seu dataset para diferentes atributos visuais (e.g. cor, forma, tamanho, textura) de sua rede.
